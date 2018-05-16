e1ea49d9aa3472c2caa4d0d8a5bf22b411de4dbd
JeanMarcGoepfert <goepfertjeanmarc@gmail.com>
2018-05-16 23:02:24 +1000

    Add initial webpack configuration with elm loader

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'static')
+  },
+  module: {
+    rules: [{
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      use: {
+        loader: 'elm-webpack-loader',
+      }
+    }]
+  }
+}
