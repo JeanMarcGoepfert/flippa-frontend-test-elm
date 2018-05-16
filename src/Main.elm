e8a3361636fffb37b653f33fbaef9760640ec832
JeanMarcGoepfert <goepfertjeanmarc@gmail.com>
2018-05-16 23:36:25 +1000

    Create elm app skeleton

--- src/Main.elm
+++ src/Main.elm
@@ -4,4 +4,55 @@ import Html exposing (Html, text, div)
 
 
 main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    ()
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( (), Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Init
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Init ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     div [] [ text "Hello World" ]
