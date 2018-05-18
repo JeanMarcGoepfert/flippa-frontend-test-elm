1943e2dc81d5e49f81df9f72c8f27d7d50e70b86
JeanMarcGoepfert <goepfertjeanmarc@gmail.com>
2018-05-18 23:29:50 +1000

    Add initial items counters request

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div)
+import Html exposing (Html, text, div, span)
+import Http
+import Json.Decode as Decode
 
 
 main =
@@ -16,13 +18,26 @@ main =
 -- MODEL
 
 
+type alias Item =
+    { id : String
+    , title : String
+    , count : Int
+    }
+
+
 type alias Model =
-    ()
+    { items : List Item
+    , loading : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( (), Cmd.none )
+    ( { items = []
+      , loading = True
+      }
+    , getItems
+    )
 
 
 
@@ -31,6 +46,7 @@ init =
 
 type Msg
     = Init
+    | GetItems (Result Http.Error (List Item))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -38,6 +54,34 @@ update msg model =
     case msg of
         Init ->
             ( model, Cmd.none )
+
+        GetItems (Ok items) ->
+            ( { model | loading = False, items = items }, Cmd.none )
+
+        GetItems (Err error) ->
+            ( { model | loading = False }, Cmd.none )
+
+
+
+-- HTTTP
+
+
+getItems : Cmd Msg
+getItems =
+    Http.send GetItems (Http.get "/api/v1/counters" decodeItems)
+
+
+decodeItems : Decode.Decoder (List Item)
+decodeItems =
+    Decode.list itemDecoder
+
+
+itemDecoder : Decode.Decoder Item
+itemDecoder =
+    Decode.map3 Item
+        (Decode.field "id" Decode.string)
+        (Decode.field "title" Decode.string)
+        (Decode.field "count" Decode.int)
 
 
 
@@ -55,4 +99,17 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "Hello World" ]
+    if model.loading then
+        div [] [ text "Loading" ]
+    else
+        div []
+            (List.map (\i -> itemView i) model.items)
+
+
+itemView : Item -> Html Msg
+itemView item =
+    div []
+        [ span [] [ text ("id: " ++ item.id ++ ", ") ]
+        , span [] [ text ("title: " ++ item.title ++ ", ") ]
+        , span [] [ text ("count:  " ++ toString item.count) ]
+        ]
